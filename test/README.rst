f5a952f956d677ea92d5704c3966da96f65cf52e
trizzle21 <tdross21@gmail.com>
2016-08-27 16:46:09 -0700

    untracked pyc, added apps, config, static

--- test/README.rst
+++
@@ -1,4 +0,0 @@
-Hello world for integrations.
-
-We're learning Django :)
-
--- wakeNshake/settings.py
+++ wakeNshake/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'oauth2_provider'
+
+
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'wakeNshake.urls'
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'wakeNshake.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
