cbdd2ba04470cc2db4e7d0062edd104670993dd3
trizzle21 <tdross21@gmail.com>
2016-08-28 08:59:30 -0700

    fixed up js

---
+++ static/js/dashboard.js
@@ -0,0 +1,25 @@
+
+
+(function(){
+	init: function(){
+		this.cacheDOM();
+		this.bindEvents();
+		this.render();
+	},
+	cacheDOM: function(){
+
+	},
+	bindEvents : function() {
+
+	},
+	render : function(){
+
+	}
+
+
+}
+	
+	desks.init();
+
+
+})()
